e58e400fb53f668c6246b99efb189631f3d634c6
Aurøra <azgath.gaming@gmail.com>
2026-07-30 22:22:34 +0200

    lua: Update GPS Darkmaul Citadel

--- lua_scripts/src/server/game/Entities/Creature/IsleReach/Alliance/CitadelleCognenuitAlliance.lua
+++ lua_scripts/src/server/game/Entities/Creature/IsleReach/Alliance/CitadelleCognenuitAlliance.lua
@@ -2,7 +2,7 @@
 local QUEST_ID = 55992
 local NPC_QUEST_STARTER = 156280
 local MAP_ID = 863
-local X, Y, Z, O = 898.273, -1771.58, 181.178, 3.6799
+local X, Y, Z, O = 4002.46, 8399.58, 322.217, 0.886692
 
 local function OnQuestAccept(event, player, creature, quest, opt)
     if quest:GetId() == QUEST_ID then
--- lua_scripts/src/server/game/Entities/Creature/IsleReach/Horde/CitadelleCognenuitHorde.lua
+++ lua_scripts/src/server/game/Entities/Creature/IsleReach/Horde/CitadelleCognenuitHorde.lua
@@ -2,7 +2,7 @@
 local QUEST_ID = 59984
 local NPC_QUEST_STARTER = 166573
 local MAP_ID = 863
-local X, Y, Z, O = 898.273, -1771.58, 181.178, 3.6799
+local X, Y, Z, O = 4002.46, 8399.58, 322.217, 0.886692
 
 local function OnQuestAccept(event, player, creature, quest, opt)
     if quest:GetId() == QUEST_ID then
